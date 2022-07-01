3ab536f00c0a7cdb8425c771a105de06a1c3d2c4
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-01 14:16:59 +0300

    2

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,14 @@
 "use strict";
-const amountCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${amountCategories.length}`);
+
+const amountCategoriesEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${amountCategoriesEl.length}`);
+console.log(amountCategoriesEl);
+
+const everyElementsEl = [...amountCategoriesEl];
+console.log(everyElementsEl);
+
+// const firstEl = amountCategoriesEl[0].querySelector(".textContent");
+// console.log( firstEl);
+// const firstCategoriesEl = amountCategoriesEl[0].map(`Category:${}`)
+
+// console.log(firstCategoriesEl);
