6af2a8fe8dc2135e5c36de1e36f49a4bc270d25c
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-02 16:22:26 +0300

    task1 end

--- js/task-01.js
+++ js/task-01.js
@@ -2,13 +2,9 @@
 
 const amountCategoriesEl = document.querySelectorAll(".item");
 console.log(`Number of categories: ${amountCategoriesEl.length}`);
-console.log(amountCategoriesEl);
+// console.log(amountCategoriesEl);
 
-const everyElementsEl = [...amountCategoriesEl];
+const everyElementsEl = [...amountCategoriesEl].map(categories => `Category: ${categories.children[0].textContent}
+Elements: ${categories.children[1].children.length} `).join("\n");
 console.log(everyElementsEl);
 
-// const firstEl = amountCategoriesEl[0].querySelector(".textContent");
-// console.log( firstEl);
-// const firstCategoriesEl = amountCategoriesEl[0].map(`Category:${}`)
-
-// console.log(firstCategoriesEl);
