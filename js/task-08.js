1748990fae3db95be7df7cff310e1e763903666c
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-10 17:12:40 +0300

    8 end

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,18 @@
 const formEl = document.querySelector(".login-form");
 const inputAll = document.querySelectorAll("input");
-console.log(inputAll);
 
 const emailEl = inputAll[0];
 const passwordEl = inputAll[1];
 
-
-function handleSubmit(event) {
+  const handleSubmit = (event) => {
   event.preventDefault();
   const formData = {
     emailEl: emailEl.value,
     passwordEl: passwordEl.value,
   };
+    if (emailEl.value === "" || passwordEl.value === "" ) {
+      alert(" Будь ласка, заповніть порожні поля ");
+    }
 
   console.log(formData);
   formEl.reset();
@@ -19,3 +20,16 @@ function handleSubmit(event) {
 
 formEl.addEventListener("submit", handleSubmit);
 
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const formData = {
+//     emailEl: emailEl.value,
+//     passwordEl: passwordEl.value,
+//   };
+
+//   console.log(formData);
+//   formEl.reset();
+// };
+
+// formEl.addEventListener("submit", handleSubmit);
