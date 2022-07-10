42015ede27886c133f3b2928e884d7fb8659bd22
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-10 19:48:18 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+console.log(buttonEl);
+const spanEl = document.querySelector(".color");
+console.log(spanEl);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+buttonEl.addEventListener("click", getRandomHexColor);
+
