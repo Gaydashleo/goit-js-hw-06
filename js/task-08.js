5df3a4628a32d83dd7506bdb4547d318a8ab1e31
Leonid Haidash <Gaydashleo@gmail.com>
2022-07-10 16:48:45 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+const inputAll = document.querySelectorAll("input");
+console.log(inputAll);
+
+const emailEl = inputAll[0];
+const passwordEl = inputAll[1];
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    emailEl: emailEl.value,
+    passwordEl: passwordEl.value,
+  };
+
+  console.log(formData);
+  formEl.reset();
+};
+
+formEl.addEventListener("submit", handleSubmit);
 
